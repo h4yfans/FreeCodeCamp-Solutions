529ec19e4f22f1b2932837c863bafcd3a60a927f
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-19 23:35:14 +0300

    Sum All Number in a Range

---
+++ Front End Development Certification/11 - Intermediate Algorithm Scripting/Sum All Numbers in a Range.js
@@ -0,0 +1,18 @@
+
+ /*
+   + Created by Kaan Karaca on 19/06/2016
+   + github.com/h4yfans
+   + feecodecamp.com/h4yfans
+   + kaan94karaca@gmail.com
+*/
+
+
+ function sumAll(arr) {
+     var sortedArr = arr.sort((a,b) => a-b);
+     var first = arr[0];
+     var last = arr[1];
+     var sum = (last-first+1) * (first + last)/2;
+     return sum;
+ }
+
+ sumAll([1, 4]);
