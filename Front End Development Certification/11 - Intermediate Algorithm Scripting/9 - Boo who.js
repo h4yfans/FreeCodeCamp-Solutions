cfb37a5a3288d236ce8a99ceb03258ccdf4bc2dc
Kaan Karaca <kaan94karaca@gmail.com>
2016-06-20 23:32:40 +0300

    Boo who FreeCodeCamp

---
+++ Front End Development Certification/11 - Intermediate Algorithm Scripting/9 - Boo who.js
@@ -0,0 +1,14 @@
+
+ /*
+   + Created by Kaan Karaca on 20/06/2016
+   + github.com/h4yfans
+   + feecodecamp.com/h4yfans
+   + kaan94karaca@gmail.com
+*/
+
+ function booWho(bool) {
+     // What is the new fad diet for ghost developers? The Boolean.
+     return typeof bool === 'boolean';
+ }
+
+ booWho(null);
